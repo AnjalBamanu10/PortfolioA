2b9c3e626b8837f608b3457431e409836095a792
AnjalBamanu10 <anjalbamanu@gmail.com>
2023-08-08 14:24:22 +0545

    new figma design

--- js/Projects.js
+++ js/Projects.js
@@ -10,7 +10,7 @@ function changeContent(page, event) {
             <div class="image-content" style="height: 100%;width:100%;">
                 <img src="img/Ecommerce_Website.jpg" alt="" style="object-fit: cover;">
                 <div class="img-content" >
-                    <p class="paragraph" style="color:white">Food Ordering App(Prototype)</p><br>
+                    <p class="paragraph" style="color:white">E-Commerce Website</p><br>
                     <p class="desc" style="color:white">Figma</p>
                 </div>
             </div>       
@@ -37,15 +37,15 @@ function changeContent(page, event) {
                 <div style="display:flex;flex-direction:column;">
                     <div class="project-box three">
                         <div class="image-content" style="height: 100%;width:100%;">
-                            <img src="img/Front_View.jpg" alt="" style="object-fit: cover;">
+                            <img src="img/Fitness.png" alt="" style="object-fit: contain;">
                             <div class="img-content" >
-                                <p class="paragraph" style="color:white">Food Ordering App(Prototype)</p><br>
+                                <p class="paragraph" style="color:white">Fitness Tracker(Ongoing Project)</p><br>
                                 <p class="desc" style="color:white">Figma</p>
                             </div>
                         </div>       
                      </div>
                      <div class="buttons project">
-                     <h2 class="pbtn"><a target="_blank" href="https://www.figma.com/file/2ATj8rp7lNCLhDFaU8Xa9H/Food-Ordering-App?type=design&node-id=0%3A1&mode=design&t=i0qAkCjpZxn4D5N7-1">Preview</a></h2>
+                     <h2 class="pbtn"><a target="_blank" href="https://www.figma.com/file/EiZAEJcBghxSud4sz9mtLa/Fitness-Track?type=design&node-id=0%3A1&mode=design&t=6Z0XXPpt3xgDUYng-1">Preview</a></h2>
                      </div>
                 </div>
             `;
@@ -56,7 +56,7 @@ function changeContent(page, event) {
              <div class="image-content" style="height: 100%;width:100%;">
                  <img src="img/Front_View.jpg" alt="" style="object-fit: cover;">
                  <div class="img-content" >
-                     <p class="paragraph" style="color:white">Food Ordering App(Prototype)</p><br>
+                     <p class="paragraph" style="color:white">fitness Tracker(Ongoing Project)</p><br>
                      <p class="desc" style="color:white">Figma</p>
                  </div>
              </div>       
@@ -89,7 +89,23 @@ function changeContent(page, event) {
       `;
         break;
     case "php":
-        content = "php";
+        content = `   <div style="display:flex;flex-direction:column;">
+          
+        <div style="display:flex;flex-direction:column;">
+        <div class="project-box4">
+            <div class="image-content" style="height: 100%;width:100%;">
+                <img src="img/Suvidha.png" alt="" style="object-fit: cover;">
+                <div class="img-content" >
+                    <p class="paragraph" style="color:white">Food Ordering App(Prototype)</p><br>
+                    <p class="desc" style="color:white">Figma</p>
+                </div>
+            </div>       
+         </div>
+         <div class="buttons project">
+         <h2 class="pbtn"><a target="_blank" href="https://www.figma.com/file/2ATj8rp7lNCLhDFaU8Xa9H/Food-Ordering-App?type=design&node-id=0%3A1&mode=design&t=i0qAkCjpZxn4D5N7-1">Preview</a></h2>
+         </div>
+    </div>
+    `;
         break;
     default:
         content = "Some error occured. Please try again!";
